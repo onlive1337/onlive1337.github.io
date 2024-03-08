46f5fab5ca44b6f4449ba76991c02ae6f0247c1f
onlive <tabdulov2006@gmail.com>
2024-03-08 11:54:28 +0500

    Update text.js
    text changes

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["music", "coding", "hugs", "playing", "chatting about feelings"];
+const textArray = ["music", "coding", "hugs", "games", "her?"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000;
